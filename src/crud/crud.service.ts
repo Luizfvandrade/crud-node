302f8d6fe9156cf4e0ab95f1b532b02fe1853615
Lzpp3 <luizfelippevieira@outlook.com>
2021-01-18 16:49:34 -0300

    feat(crud): write crude calling redis

--- src/crud/crud.service.ts
+++ src/crud/crud.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+import { RedisService } from '../redis/redis.service';
+
+import { GenericBody } from './crud.interface';
+
 @Injectable()
-export class CrudService {}
+export class CrudService {
+
+    constructor(
+        private readonly redisService: RedisService
+    ){ }
+
+    async create(body: GenericBody): Promise<any> {
+        const response = await this.redisService.set(body.cpf, body)
+        return response
+    }
+
+    async read(cpf: string): Promise<any> {
+        const response = await this.redisService.get(cpf)
+        return response
+    }
+
+    async update(cpf: string, body: GenericBody): Promise<any> {
+        if(cpf !== body.cpf) throw new Error('Id not match')
+        
+        const response = await this.redisService.set(cpf, body)
+        return response
+    }
+
+    async delete(cpf: string): Promise<any> {
+        const response = await this.redisService.del(cpf)
+        return response
+    }
+}
