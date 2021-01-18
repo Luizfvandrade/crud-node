d27ecefb20ea101eeac93e7e0904331486dc80d7
Lzpp3 <luizfelippevieira@outlook.com>
2021-01-18 16:49:48 -0300

    feat(app): minor changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: false,
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
   });
 
   const port = process.env.PORT || 2929;
