8da2186020b7e29268ef19854e308c30d380e64c
Lzpp3 <luizfelippevieira@outlook.com>
2021-01-18 16:22:34 -0300

    feat(app): minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CrudController } from './crud/crud.controller';
+
+import { AppService } from './app.service';
 import { CrudService } from './crud/crud.service';
 import { ValidatorService } from './validator/validator.service';
 import { RedisService } from './redis/redis.service';
 
+const env = process.env.NODE_ENV
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: `./config/.env.${env}`,
+    isGlobal: true,
+  }),],
   controllers: [AppController, CrudController],
   providers: [AppService, CrudService, ValidatorService, RedisService],
 })
