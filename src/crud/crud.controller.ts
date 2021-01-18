acfc8136ddd62e0744024e1aa68bbdd8bd6f8ee4
Lzpp3 <luizfelippevieira@outlook.com>
2021-01-18 16:49:12 -0300

    feat(crud): write crud routes

--- src/crud/crud.controller.ts
+++ src/crud/crud.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 
 import { ValidatorService } from '../validator/validator.service';
 
 import { GenericBody } from './crud.interface';
+import { CrudService } from './crud.service';
 
 @Controller('crud')
 export class CrudController {
     constructor(
+        private readonly crudService: CrudService,
         private readonly validatorService: ValidatorService,
-
     ){}
 
     @HttpCode(HttpStatus.CREATED)
@@ -16,31 +17,55 @@ export class CrudController {
     async create(
         @Body() request: GenericBody,
     ): Promise<any>{
+        try {
+            const response = await this.crudService.create(request)
 
+            return response
+        } catch(error) {
+            throw error
+        }
     }
 
     @HttpCode(HttpStatus.OK)
-    @Get('id')
+    @Get(':id')
     async read(
         @Param('id') cpf: string,
     ): Promise<any>{
+        try {
+            const response = await this.crudService.read(cpf)
 
+            return response
+        } catch(error) {
+            throw error
+        }
     }
 
     @HttpCode(HttpStatus.OK)
-    @Put('id')
+    @Put(':id')
     async update(
         @Param('id') cpf: string,
         @Body() request: GenericBody,
     ): Promise<any>{
+        try {
+            const response = await this.crudService.update(cpf, request)
 
+            return response
+        } catch(error) {
+            throw error
+        }
     }
 
     @HttpCode(HttpStatus.NO_CONTENT)
-    @Put()
+    @Delete(':id')
     async delete(
         @Param('id') cpf: string,
     ): Promise<any>{
+        try {
+            const response = await this.crudService.delete(cpf)
 
+            return response
+        } catch(error) {
+            throw error
+        }
     }
 }
